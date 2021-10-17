f5868ba4d313545aa62ffdc0b7d519255da503de
c0ndi <kowalik0konrad@gmail.com>
2021-10-17 21:58:32 +0200

    Loading Screen

--- components/landing/LoadingScreen.tsx
+++ components/landing/LoadingScreen.tsx
@@ -1,8 +1,23 @@
+import Fade from 'react-reveal/Fade'
+
 export default function LoadingScreen(): JSX.Element {
     return (
-        <div>
+        <div className="w-100 h-screen  grid place-items-center ">
             <div>
-                <span>Send</span>
+                <span className="font-extrabold font-montserrat text-6xl select-none scale-animation flex">
+                    <span className="text-black">
+                        <span>Send</span>
+                        <div className="border-b-4 border-black mt-2 borderAnimationOne"></div>
+                    </span>
+                    <span className="text-main">
+                        <span>Your</span>
+                        <div className="w-0 border-b-4 border-main mt-2 borderAnimationTwo"></div>
+                    </span>
+                    <span className="text-black">
+                        <span>Work</span>
+                        <div className="w-0 border-b-4 border-black mt-2 borderAnimationThree"></div>
+                    </span>
+                </span>
             </div>
         </div>
     )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,32 @@
 import Navbar from '../components/landing/Navbar'
 import Main from '../components/landing/Main'
 import ProjectInfo from '../components/landing/ProjectInfo'
+import LoadingScreen from '../components/landing/LoadingScreen'
 import Head from 'next/head'
+import { useState, useEffect } from 'react'
 
 const Page = () => {
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false);
+        }, 1500)
+    })
     return (
         <>
             <Head>
                 <title>sendYourwork</title>
             </Head>
-            <Navbar />
-            <Main />
-            <ProjectInfo />
+            {loading ?
+                <LoadingScreen />
+                :
+                <>
+                    <Navbar />
+                    <Main />
+                    <ProjectInfo />
+                </>
+
+            }
         </>
     )
 }

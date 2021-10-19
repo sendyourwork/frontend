be09845299ea4d3361d90d960408fc6814a3a7db
c0ndi <kowalik0konrad@gmail.com>
2021-10-19 20:47:20 +0200

    Update Main.tsx

--- components/landing/Main.tsx
+++ components/landing/Main.tsx
@@ -9,7 +9,7 @@ interface LinkButtonProps {
 
 export default function Main({ linkProps }: LinkButtonProps): JSX.Element {
     return (
-        <div className="flex h-85vh font-montserrat relative flex-col lg:flex-row items-center xl:items-start overflow-x-hidden">
+        <main className="flex h-85vh font-montserrat relative flex-col lg:flex-row items-center xl:items-start overflow-x-hidden">
             <Fade top delay={300}>
                 <img src="/Star 6.svg" className="left-4 transform hover:rotate-180 transition duration-500 hidden lg:block absolute" />
                 <img src="/Rectangle90.svg" className="top-3/4 left-1/4 transform transition duration-500 hidden lg:block absolute" />
@@ -18,9 +18,9 @@ export default function Main({ linkProps }: LinkButtonProps): JSX.Element {
                 <img src="/Rectangle94.svg" className="right-12 top-3/4 transform transition duration-500 hidden lg:block absolute" />
             </Fade>
             <Fade left delay={700}>
-                <div className="flex h-5/6 justify-center ">
+                <div className="flex h-5/6 justify-center">
                     <div className="flex flex-col w-7/12 h-full justify-center mt-12 md:mt-0">
-                        <span className="font-montserrat font-extrabold text-3xl md:text-6xl">Send your homework safe and simple</span>
+                        <span className="font-montserrat font-extrabold text-4xl md:text-6xl">Send your homework safe and simple</span>
                         <span className="mt-4 text-m font-medium">We are help education in the world. We make world a better place.</span>
                         <div className="mt-4">
                             <Link href={linkProps} passHref={true}>
@@ -35,6 +35,6 @@ export default function Main({ linkProps }: LinkButtonProps): JSX.Element {
                     <img src="/Group 146.png" className="myShadow" />
                 </div>
             </Fade>
-        </div >
+        </main>
     )
 }

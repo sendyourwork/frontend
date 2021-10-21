6dc83cb8ba07413c4061376033b349be21550a60
c0ndi <kowalik0konrad@gmail.com>
2021-10-21 19:16:11 +0200

    Select to class

--- components/AddFile.tsx
+++ components/AddFile.tsx
@@ -6,7 +6,7 @@ type AddFileProps = {
     add: (newFiles: FileI[] | File[]) => void,
     fetchFunction?: (files: FormData) => Promise<any>
 }
-export default function AddFile({add, fetchFunction}: AddFileProps): JSX.Element {
+export default function AddFile({ add, fetchFunction }: AddFileProps): JSX.Element {
     const fileInputRef = useRef<HTMLInputElement>(null);
     const [error, setError] = useState('');
     const [isUploading, setIsUploading] = useState(false);
@@ -17,27 +17,26 @@ export default function AddFile({add, fetchFunction}: AddFileProps): JSX.Element
         fileInputRef.current?.click();
     }
     const checkIsFolder = (files: FileList): Boolean => {
-        for(let i = 0; i < files.length; i++) {
-            if(!files[i].name.includes('.'))
-            {
+        for (let i = 0; i < files.length; i++) {
+            if (!files[i].name.includes('.')) {
                 return true;
             }
         }
         return false;
     }
     const checkAndSend = async (files: FileList | null) => {
-        if(files && files.length){
-            if(checkIsFilesSizeNotTooBig(files)){
+        if (files && files.length) {
+            if (checkIsFilesSizeNotTooBig(files)) {
                 setError("");
-                if(!checkIsFolder(files)) {  
-                    if(fetchFunction) {  
-                        setIsUploading(true);         
+                if (!checkIsFolder(files)) {
+                    if (fetchFunction) {
+                        setIsUploading(true);
                         const filesToSend = new FormData();
-                        for(let i = 0; files.length > i; i++){
+                        for (let i = 0; files.length > i; i++) {
                             filesToSend.append("files", files[i]);
                         }
                         const response = await fetchFunction(filesToSend);
-                        if(response?.status) {
+                        if (response?.status) {
                             add(response.data);
                             setIsUploading(false);
                         }
@@ -48,7 +47,7 @@ export default function AddFile({add, fetchFunction}: AddFileProps): JSX.Element
                     }
                     else {
                         const filesToAdd = [];
-                        for(let i = 0; files.length > i; i++){
+                        for (let i = 0; files.length > i; i++) {
                             filesToAdd.push(files[i]);
                         }
                         add(filesToAdd);
@@ -76,22 +75,21 @@ export default function AddFile({add, fetchFunction}: AddFileProps): JSX.Element
 
     const handleDragOver = (event: React.DragEvent<HTMLDivElement>) => {
         preventDefaultDragEvent(event);
-        setAddFileViewVisible(true);   
+        setAddFileViewVisible(true);
     }
 
     const handleDragLeave = (event: React.DragEvent<HTMLDivElement>) => {
-        if(enterTarget == event.target)
-        {
+        if (enterTarget == event.target) {
             preventDefaultDragEvent(event);
             setAddFileViewVisible(false);
-        }  
+        }
     }
 
     const handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
         preventDefaultDragEvent(event);
         const files = event.dataTransfer.files;
         checkAndSend(files);
-        setAddFileViewVisible(false);  
+        setAddFileViewVisible(false);
     }
 
     const handleChangeFile = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -108,18 +106,18 @@ export default function AddFile({add, fetchFunction}: AddFileProps): JSX.Element
             onDragLeave={handleDragLeave}
             className="p-5 myShadow rounded relative"
         >
-            <input type="file" ref={fileInputRef} className="w-0" onChange={handleChangeFile} multiple/>
+            <input type="file" ref={fileInputRef} className="w-0" onChange={handleChangeFile} multiple />
             <button onClick={handleAddFileClick} className="text-blue-600 hover:text-blue-500 hover:cursor-pointer">Select a file</button>
             <span> or drag in form</span>
-            {error ? 
+            {error ?
                 <p className="text-red-500 text-sm">{error}</p>
                 :
                 <p className="text-gray-400 text-sm">PNG, jpg, gif files up to 10 MB in size are available for download</p>
             }
             {addFileViewVisible &&
-            <div className="absolute w-full h-full flex items-center justify-center top-0 left-0 bg-white border-2 border-black border-dashed rounded">
-                <svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 0 24 24" width="48px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/></svg>
-            </div>
+                <div className="absolute w-full h-full flex items-center justify-center top-0 left-0 bg-white border-2 border-black border-dashed rounded">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 0 24 24" width="48px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none" /><path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" /></svg>
+                </div>
             }
             {isUploading &&
                 <div className="absolute left-0 top-0 w-full h-full bg-white bg-opacity-80 flex items-center justify-center">

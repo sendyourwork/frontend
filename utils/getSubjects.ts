44165c7067c307c2da9037f9e76e3f3d14aa85bb
Artiu <oborskiartur123@gmail.com>
2021-10-22 11:41:05 +0200

    commit

---
+++ utils/getSubjects.ts
@@ -0,0 +1,10 @@
+import { BACKEND_URL } from "../config";
+
+export default function getSubjects(school_class: string) {
+    return fetch(BACKEND_URL + "/subjects/" + school_class, {
+        headers: {
+            "Authorization": "Bearer " + localStorage.getItem('token')
+        }
+    })
+    .then(response => response.json())
+}
--- utils/homeDriveFileDownload.ts
+++ utils/homeDriveFileDownload.ts
@@ -1,7 +1,7 @@
 import { BACKEND_URL } from "../config";
 
 export default function homeDriveFileDownload(filename: string, username: string) {
-    return fetch(BACKEND_URL + "/" + username + "/" + filename, {
+    return fetch(BACKEND_URL + "/userfiles/" + username + "/" + filename, {
         headers: {
             "Authorization": "Bearer " + localStorage.getItem('token')
         }

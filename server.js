4f5d96c5d0e547fdd54ec6a0f2e6cb8704d4dd4f
some0ne3 <53973517+some0ne3@users.noreply.github.com>
2021-10-23 23:01:43 +0000

    Add server.js for deploying with SSL

---
+++ server.js
@@ -0,0 +1,27 @@
+const { createServer } = require("https");
+const { parse } = require("url");
+const next = require("next");
+const fs = require("fs");
+const dev = false;
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/send-your.works/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/send-your.works/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/send-your.works/chain.pem', 'utf8');
+
+const credentials = {
+    key: privateKey,
+    cert: certificate,
+    ca: ca
+};
+
+app.prepare().then(() => {
+    createServer(credentials, (req, res) => {
+        const parsedUrl = parse(req.url, true);
+        handle(req, res, parsedUrl);
+    }).listen(443, (err) => {
+        if (err) throw err;
+        console.log("Started server");
+    });
+});
